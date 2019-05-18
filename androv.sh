7c71e9ac2edc4b51310d9b6d76594e120cdfea6c
tampansky <42198256+tampansky@users.noreply.github.com>
2019-05-18 22:07:43 +0700

    Update

--- androv.sh
+++ androv.sh
@@ -4,16 +4,16 @@
 figlet Tampansky | lolcat
 figlet -f emboss LORD | lolcat
 echo
-echo '----------------------------------------'| lolcat
-echo '[AUTHOR : TAMPANSKY]'| lolcat
-echo '[SUPORTED BY : MY FAMILY]'| lolcat
-echo '[MY TEAM : RED EYE CYBER TEAM]'| lolcat
-echo '[MY contek : 087887986528]'| lolcat
-echo '[THANKS YOU MY GIRLFRIENDS And My MOTHER]'| lolcat
-echo '[FOR SUPORTED MY :)]'| lolcat
-echo '-----------------------------------------'| lolcat
+echo "----------------------------------------"| lolcat
+echo "[AUTHOR : TAMPANSKY]"
+echo "[SUPORTED BY : MY FAMILY]"
+echo "[MY TEAM : RED EYE CYBER TEAM]"
+echo "[MY contek : 087887986528]"
+echo "[THANKS YOU MY GIRLFRIENDS And My MOTHER]"
+echo "[FOR SUPORTED MY :)]"
+echo "-----------------------------------------'| lolcat
 echo
-sleep 0.01
+sleep 0.100
 git clone https://github.com/ashishb/android-malware
 echo -e "${y} Cara membuat virus mematikan"
 echo -e "${y} cd android-malware"
