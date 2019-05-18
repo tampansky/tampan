e88d38fc8fdebc3470d0c6a1c03b97d74c059d47
tampansky <42198256+tampansky@users.noreply.github.com>
2019-05-18 21:41:14 +0700

    Add files via upload

---
+++ androv.sh
@@ -0,0 +1,24 @@
+#TOOLS PRIVAT
+#colors
+
+figlet Tampansky | lolcat
+figlet -f emboss LORD | lolcat
+echo
+echo '----------------------------------------'| lolcat
+echo '[AUTHOR : TAMPANSKY]'| lolcat
+echo '[SUPORTED BY : MY FAMILY]'| lolcat
+echo '[MY TEAM : RED EYE CYBER TEAM]'| lolcat
+echo '[MY contek : 087887986528]'| lolcat
+echo '[THANKS YOU MY GIRLFRIENDS And My MOTHER]'| lolcat
+echo '[FOR SUPORTED MY :)]'| lolcat
+echo '-----------------------------------------'| lolcat
+echo
+sleep 0.01
+git clone https://github.com/ashishb/android-malware
+echo -e "${y} Cara membuat virus mematikan"
+echo -e "${y} cd android-malware"
+echo -e "${y} ls"
+echo -e "${y} Nah kalian tinggal masuk ke dictiory virusnya"
+echo -e "${y} Contoh = cd xbot"
+echo -e "${y} ls"
+echo -e "${y} Nah kalian tinggal pindahin virus itu ke sdcard"
