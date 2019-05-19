a8f7058ff73d78722df2894abd5c7cf60c2086bb
tampansky <42198256+tampansky@users.noreply.github.com>
2019-05-19 13:36:29 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,37 +9,37 @@ red='\e[1;31m'
 
 echo -e $cyan "Installasi..."
 echo -e $red "Loading..."
-apt update
+apt-get update
 pkg update
 apt-get install ruby
 pkg install ruby
-apt upgrade -y
+apt-get upgrade -y
 pkg upgrade -y
-apt install nmap -y
+apt-get install nmap -y
 pkg install nmap -y
-apt install python
+apt-get install python
 pkg install python
-apt install python2
+apt-get install python2
 pkg install python2
-apt install wget -y
+apt-get install wget -y
 pkg install wget -y
-apt install lolcat
+apt-get install lolcat
 pkg install lolcat
-apt install figlet
+apt-get install figlet
 pkg install figlet
-apt install cowsay
+apt-get install cowsay
 pkg install cowsay
 gem install lolcat
 pip2 install enlighten
-apt install curl -y
+apt-get install curl -y
 pkg install curl -y
-apt install php -y
+apt-get install php -y
 pkg install php -y
-apt install git -y
+apt-get install git -y
 pkg install git -y
-apt install python -y
+apt-get install python -y
 pkg install python -y
-apt install python2 -y
+apt-get install python2 -y
 pip install --upgrade pip
 pip install requests
 pip install bs4
